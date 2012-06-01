81361dda779f5a9d0c5fbc2268017f07a9537888
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-06-01 14:53:10 -0500

    adding groovy geb script example to start with

---
+++ xythos_upload.groovy
@@ -0,0 +1,42 @@
+@Grapes([
+	@Grab("org.codehaus.geb:geb-core:0.7.0"),
+	@Grab("org.seleniumhq.selenium:selenium-firefox-driver:2.15.0"),
+	@Grab("org.seleniumhq.selenium:selenium-support:2.15.0")])
+
+Import geb.Browser
+ 
+// ... some examples ...
+Browser.drive {
+	go "http://myapp.com/login"
+
+	assert $("h1").text() == "Please Login"
+
+	$("form.login").with {
+		username = "admin"
+		password = "password"
+		login().click()
+	}
+
+	assert $("h1").text() == "Admin Section"
+}
+
+Browser.drive {
+	go 'https://www.ncs.umn.edu/manageLists/list'
+
+	waitFor { title == "Sign In: University of Minnesota" }
+
+	$("#lform").with {
+		j_username = "username"
+		j_password = "somepassword"
+		sleep 4000
+		$("input", type:"submit").click()
+	}
+
+	waitFor { title == "Mailing Lists" }
+
+	go '/manageLists/show/NCS_UMN_Info'
+
+	assert $("span.fullName", 0).text() == "Amy Arellano"
+
+}
+
